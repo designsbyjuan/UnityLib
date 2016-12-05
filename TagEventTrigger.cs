44f0e4e8af36463183a40128f1a334afffb6f667
Juan Paolo Mendiola <designsbyjuan@gmail.com>
2016-12-05 15:01:00 -0500

    adding to repo

---
+++ TagEventTrigger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TagEventTrigger : ButtonEventTrigger {
+
+  protected void Awake()
+  {
+    base.Awake();
+  }
+	// Use this for initialization
+	protected void Start () 
+  {
+	  base.Start();
+	} 
+
+  protected override void OnPointerDownDelegate(PointerEventData data) { /* do nothing */ }
+
+}
